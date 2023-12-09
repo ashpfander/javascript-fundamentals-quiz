a52750e429de4d9e0c9dcfea7f9425a4b2897236
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2023-12-09 12:18:10 -0500

    added CSS styling to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,31 @@
 // Traverse the DOM to select certain areas in HTML
-var quizBegin = document.querySelector(".intro");
-var startButton = document.querySelector(".start-button");
 var timer = document.querySelector(".timer");
-var quizQuestions = document.querySelector(".quiz-questions");
-var addInitials = document.querySelector(".end-quiz");
-var highscoreSubmit = document.querySelector(".add-initials-button");
+var quizArea = document.querySelector(".quiz-area");
+var title = document.querySelector(".title");
+var questionsText = document.querySelector(".questions-text");
+var button = document.querySelector(".button");
 
 // Create questions in arrays
 var questions = [
     {
         question: "Who invented Javascript?",
         options: ["Chris Beard", "Guido van Rossum", "Brendan Eich", "Larry Page"],
-        answer: "Brendan Eich"
+        answer: 2
     },
     {
         question: "What is a primitive data type?",
         options: ["Number", "String", "Boolean", "All of the above"],
-        answer: "All of the above"
+        answer: 3
     },
     {
         question: "How can you refer to a global object?",
         options: ["this", "that", "there", "those"],
-        answer: "this"
+        answer: 0
     },
     {
         question: "How can you declare a variable?",
         options: ["var", "let", "const", "All of the above"],
-        answer: "All of the above"
+        answer: 3
     }
   ];
 
