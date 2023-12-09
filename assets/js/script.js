c4311c65e4ce1eb3b42e28986b181196e301736d
Ashleigh Pfander <ashleighharvey@Ashleighs-MBP.lan>
2023-12-08 20:43:38 -0500

    added some HTML and javascript

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// Traverse the DOM to select certain areas in HTML
+var quizBegin = document.querySelector(".intro");
+var startButton = document.querySelector(".start-button");
+var questions = document.querySelector(".quiz-questions");
+var addInitials = document.querySelector(".end-quiz");
+var highscoreSubmit = document.querySelector(".add-initials-button");
+
+// Create questions in arrays
+// question1: Who invented Javascript?
+    // a: Chris Beard
+    // b: Guido van Rossum
+    // c: Brendan Eich
+    // d: Larry Page
+        // Correct answer: C
+
+// question2: What is a primitive data type?
+    // a: Number
+    // b: String
+    // c: Boolean
+    // d: All of the above
+        // Correct answer: D
+
+// question3: How can you refer to a global object?
+    // a: this
+    // b: that
+    // c: there
+    // d: those
+        // Correct answer: A
+
+// question4: How can you declare a variable?
+    // a: var
+    // b: let
+    // c: const
+    // d: All of the above
+        // Correct answer: D
